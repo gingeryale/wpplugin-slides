1fb111937c8b58201e82cfcbde4f5d31ed06326d
ao <aolin@ymail.com>
2023-01-03 22:31:31 +0200

    sanitized form data

--- mv-slider/views/mv-slider_metabox.php
+++ mv-slider/views/mv-slider_metabox.php
@@ -1,3 +1,9 @@
+<?php
+$meta = get_post_meta($post->ID);
+//var_dump($meta);
+$link_text = $meta['mv_slider_link_text'][0];
+$link_url = $meta['mv_slider_link_url'][0];
+?>
 <table class="form-table mv-slider-metabox">
     <tbody>
         <tr>
@@ -5,7 +11,7 @@
                 <label for="mv_slider_link_text">link Text</label>
             </th>
             <td>
-                <input type="text" id="mv_slider_link_text" name="mv_slider_link_text" value="" class="regular-text link-text" required>
+                <input type="text" id="mv_slider_link_text" name="mv_slider_link_text" value="<?php echo (isset($link_text)) ? esc_html($link_text) : ''; ?>" class="regular-text link-text" required>
             </td>
         </tr>
         <tr>
@@ -13,7 +19,7 @@
                 <label for="mv_slider_link_url">link URL</label>
             </th>
             <td>
-                <input type="url" id="mv_slider_link_url" name="mv_slider_link_url" value="" class="regular-text link-url" required>
+                <input type="url" id="mv_slider_link_url" name="mv_slider_link_url" value="<?php echo (isset($link_url)) ? esc_url($link_url) : ''; ?>" class="regular-text link-url" required>
             </td>
         </tr>
     </tbody>
