f695f8a23b30b01caf91cdb2e3e1ba56907f56e2
ao <aolin@ymail.com>
2023-01-10 09:34:50 +0200

    options settings page division and active css

--- mv-slider/class.mv-slider-settings.php
+++ mv-slider/class.mv-slider-settings.php
@@ -117,6 +117,7 @@ if (!class_exists('MV_Slider_Settings')) {
                 switch ($key) {
                     case 'mv_slider_title':
                         if (empty($value)) {
+                            add_settings_error('mv_slider_options', 'mv_slider_message', 'Title field can\'t be empty', 'error');
                             $value = 'Please enter a value';
                         }
                         $new_input[$key] = sanitize_text_field($value);
