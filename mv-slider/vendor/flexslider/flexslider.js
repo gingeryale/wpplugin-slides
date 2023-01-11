67f912af4b289ecf82eb7d516cbb27e1c8fc5613
ao <aolin@ymail.com>
2023-01-11 10:17:39 +0200

    added flexsider vendor files

---
+++ mv-slider/vendor/flexslider/flexslider.js
@@ -0,0 +1,8 @@
+jQuery(window).load(function () {
+    jQuery('.flexslider').flexslider({
+        animation: "slider",
+        touch: true,
+        directionNav: true,
+        controlNav: true,
+    });
+});
