97876d8191bad31fbdcb23f82737c997d22caf65
ao <aolin@ymail.com>
2023-01-15 21:40:51 +0200

    placeholder img added dynamic css

--- mv-slider/views/mv-slider_shortcode.php
+++ mv-slider/views/mv-slider_shortcode.php
@@ -1,6 +1,6 @@
 <h3><?php echo (!empty($content)) ? esc_html($content) : esc_html(MV_Slider_Settings::$options['mv_slider_title']); ?></h3>
 
-<div class="mv-slider flexslider">
+<div class="mv-slider flexslider <?php echo (isset(MV_Slider_Settings::$options['mv_slider_style'])) ? esc_attr(MV_Slider_Settings::$options['mv_slider_style']) : 'style-1'; ?>">
     <ul class="slides">
         <?php
         $args = array(
@@ -16,7 +16,14 @@
                 $button_url = get_post_meta(get_the_ID(), 'mv_slider_link_url', true);
         ?>
                 <li>
-                    <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+                    <?php
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('full', array('class' => 'img-fluid'));
+                    } else {
+                        echo "<img src=' " . MV_SLIDER_URL . "assets/images/default.jpg' 
+                        class='img-fluid wp-post-image' /> ";
+                    }
+                    ?>
                     <div class="mvs-container">
                         <div class="slider-details-container">
                             <div class="wrapper">
